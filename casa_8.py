d67c4a1ab1e82f07b2fa9bc34a69872e9f1fd976
janetteea <jackerman2@miners.utep.edu>
2024-02-14 00:50:41 -0700

    Casa 8

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,24 @@
+# Method 4, using iterrows() 
+
+# Import pandas package as pd
+import pandas as pd
+
+df = pd.read_csv('big-mac-full-index.csv')
+
+# Method 4:  Using iterrows()
+
+for i,row in df.iterrows():
+    print (row['date'],
+           row['currency_code'],
+           row['name'],
+           row['dollar_ex'],
+           row['dollar_price']) 
+    
+# Method 6:  Using apply()
+
+print (df.apply(lambda row: (row['date'], row['name'], row['dollar_price']), axis = 1))
+
+
+    
+
 
